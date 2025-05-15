d9a1ddcc2a2e77fb8a175cafbd4d87cc144d7c37
GiTae Kim <132313723+GiTae-Kim-kr@users.noreply.github.com>
2025-05-16 00:09:51 +0900

    Update app.py

--- MazeShare/app.py
+++ MazeShare/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from config import Config
 from models import db, Maze  # Maze 모델도 import 필요!
+from bp import maze_bp
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -13,6 +14,9 @@ with app.app_context():
 def index():
     mazes = Maze.query.order_by(Maze.created_at.desc()).all()
     return render_template('index.html', mazes=mazes)
+
+# Blueprint 등록
+app.register_blueprint(maze_bp)
 
 if __name__ == "__main__":
     app.run(debug=True)
