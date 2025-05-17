55d554a8b10c7f15e14669f4ba0862818f4dd05f
GiTae Kim <132313723+GiTae-Kim-kr@users.noreply.github.com>
2025-05-17 18:41:00 +0900

    Update config.py

--- MazeShare/config.py
+++ MazeShare/config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(os.path.abspath(os.path.dirname(__file__)), 'instance', 'mazeshare.db')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-very-secret-key'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(os.path.abspath(os.path.dirname(__file__)), 'instance', 'maze_share.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
